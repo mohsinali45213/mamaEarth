9e78ee5bb0d5506e237d0796fced2494a9478757
mohsinali45213 <mohsinali45213@gmail.com>
2023-12-14 13:31:41 +0530

    Navbar

---
+++ Client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import Home from './Page/Home';
+import Header from './Components/Header/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+    
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    
+    </div>
+  );
+};
+
+export default App;
---
+++ Client/src/Components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Header.css"
+import "./Header.js"
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-top'>
+        <img id='logo' src="src/assets/Images/mamaearth_logo.png" alt="logo" />
+        <div className='search'>
+          <input type="search-input" />
+          <button>
+            <img id='search' src="src/assets/Images/search.png" alt="search" />
+          </button>
+        </div>
+        <div className='icons'>
+        <span>
+          <img id="icon" src="src/assets/Images/shopping-bag.png" alt="cart" />
+          <p>Cart</p>
+        </span>
+        <span>
+          <img id="icon" src="src/assets/Images/user.png" alt="account" />
+          <p>Account</p>
+        </span>
+        </div>
+      </div>
+      <div className='navbar'>
+        <ul>
+          <li><Link id='navItem' to="/">Home</Link></li>
+          <li><Link id='navItem' to="/">Hair</Link></li>
+          <li><Link id='navItem' to="/">Face</Link></li>
+          <li><Link id='navItem' to="/">Body</Link></li>
+          <li><Link id='navItem' to="/">MakeUp</Link></li>
+          <li><Link id='navItem' to="/">Ingredient</Link></li>
+          <li><Link id='navItem' to="/">Baby</Link></li>
+          <li><Link id='navItem' to="/">All Products</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
